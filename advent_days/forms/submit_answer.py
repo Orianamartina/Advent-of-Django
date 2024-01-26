ff6070b3024d9299e8aa69d693ad0312ba1084e1
Oriana <oriana@camba.coop>
2024-01-26 17:05:41 -0300

    form

--- advent_days/forms/submit_answer.py
+++ advent_days/forms/submit_answer.py
@@ -9,5 +9,5 @@ class SubmissionForm(forms.Form):
     code = forms.CharField(widget=forms.Textarea(attrs={'class':'input-input'}))
     description = forms.CharField(widget=forms.Textarea(attrs={'class':'input-input'}))
     link_to_repo = forms.CharField()
-    answer_part_one = forms.Textarea()
-    answer_part_two = forms.Textarea()
+    answer_part_one = forms.Textarea(widget=forms.Textarea(attrs={'class':'input-input'}))
+    answer_part_two = forms.Textarea(widget=forms.Textarea(attrs={'class':'input-input'}))
