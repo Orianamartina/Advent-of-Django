e8f47f2d3d769ed6d2324a715295142d510ddd42
Oriana <oriana@camba.coop>
2024-01-26 15:40:21 -0300

    db

--- advent2022/settings.py
+++ advent2022/settings.py
@@ -85,17 +85,17 @@ WSGI_APPLICATION = 'advent2022.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 # DATABASES = {
-#     'default': dj_database_url.config(        
-#         # Feel free to alter this value to suit your needs.        
-#         default='postgresql://postgres:postgres@localhost:5432/mysite',        
-#         conn_max_age=600    )}
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+DATABASES = {
+    'default': dj_database_url.config(        
+        # Feel free to alter this value to suit your needs.        
+        default='postgresql://postgres:postgres@localhost:5432/mysite',        
+        conn_max_age=600    )}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
